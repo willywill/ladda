2af96828d5edd104838a52eafc3a08cd6ed98fd9
Will Germany <germany.william@gmail.com>
2021-03-21 21:24:02 -0400

    Resolving TODOs for errors and env variables

--- cmd/main.go
+++ cmd/main.go
@@ -4,23 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/willywill/ladda/pkg/handler"
 	"github.com/willywill/ladda/pkg/middleware"
 )
 
-// TODO: Pass this as an environment variable
-const PORT int = 3001
-
 func main() {
+	port := "3001"
+	envPort, ok := os.LookupEnv("PORT")
+
+	if !ok {
+		log.Println("No port specified, using default - 3001")
+	} else {
+		port = envPort
+	}
+
 	multiplexer := http.NewServeMux()
 
 	uploadHandler := http.HandlerFunc(handler.UploadFile)
 
 	multiplexer.Handle("/", middleware.Authenticate(uploadHandler))
 
-	fmt.Printf("> Server started on port %v\n", PORT)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), multiplexer)
+	fmt.Printf("> Server started on port %v\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), multiplexer)
 
 	if err != nil {
 		log.Fatal(err)
--- pkg/handler/uploader.go
+++ pkg/handler/uploader.go
@@ -1,15 +1,24 @@
 package handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
+
+type ResponseError struct {
+	Error string `json:"error"`
+}
 
 func UploadFile(w http.ResponseWriter, req *http.Request) {
 	log.Println("Uploading File Handler Called")
 
 	if req.Method != "POST" {
-		// TODO: Send a response error
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(ResponseError{
+			Error: "This HTTP method is not supported.",
+		})
 		log.Println("Unsupported HTTP method")
 	}
 }
